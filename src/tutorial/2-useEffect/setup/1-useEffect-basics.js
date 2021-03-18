94dc6a9a499ebf4ab177fd8a26200f25b913735f
UJ528491 <hyj0829@gmail.com>
2021-03-19 01:34:01 +0900

    useEffect

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+  useEffect(() => {
+    console.log("useEffect on");
+    if (value >= 1) {
+      document.title = `New Messages (${value})`;
+    }
+    // when list of dependency value changed render
+  }, []);
+  console.log("render component");
+  return (
+    <>
+      <h1>{value}</h1>
+      <button className="btn" onClick={() => setValue(value + 1)}>
+        increase
+      </button>
+    </>
+  );
 };
 
 export default UseEffectBasics;
