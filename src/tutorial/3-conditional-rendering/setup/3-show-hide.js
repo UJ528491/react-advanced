bc36cb3534c088b5b6c20010360e8cd5d1ad0bab
UJ528491 <hyj0829@gmail.com>
2021-04-10 20:30:56 +0900

    toggle, useEffect listener

--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+  const [show, setShow] = useState(false);
+
+  return (
+    <>
+      <h2>show/hide</h2>
+      <button className="btn" onClick={() => setShow(!show)}>
+        toggle
+      </button>
+      {show && <Item />}
+    </>
+  );
+};
+const Item = () => {
+  const [size, setSize] = useState(window.innerWidth);
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", checkSize);
+    return window.removeEventListener("resize", checkSize);
+  }, []);
+  return (
+    <div style={{ marginTop: "2rem" }}>
+      <h2>window width size : {size}px</h2>
+    </div>
+  );
 };
 
 export default ShowHide;
